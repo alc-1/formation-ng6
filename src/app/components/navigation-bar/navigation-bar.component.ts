42e0a2b395644ab88c595f6484164205a2b490cf
Arnaud Béguin <abeguin@octo.com>
2019-04-07 21:38:32 +0200

    add the logout button in the navigation bar

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from 'src/app/store';
+import { Logout } from 'src/app/store/actions/login.actions';
+import { isLoggedIn$ } from 'src/app/store/selectors/login.selectors';
 
 @Component({
   selector: 'navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.css']
 })
-export class NavigationBarComponent implements OnInit {
+export class NavigationBarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  logginSubscription: Subscription;
+  loggedIn: boolean;
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.store.pipe(select(isLoggedIn$))
+      .subscribe((isLoggedIn: boolean) => {
+        this.loggedIn = isLoggedIn;
+      })
   }
 
+  get isLoggedIn(): boolean {
+    return this.loggedIn;
+  }
+
+  logout() {
+    this.store.dispatch(new Logout());
+  }
+
+  ngOnDestroy() {
+    if (this.logginSubscription) {
+      this.logginSubscription.unsubscribe();
+    }
+  }
 }
