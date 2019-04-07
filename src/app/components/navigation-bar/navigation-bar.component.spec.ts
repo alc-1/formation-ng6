cf28b2d79bb6a18509b8c57074f6ab0ee2ae7a05
Arnaud Béguin <abeguin@octo.com>
2019-04-07 19:10:22 +0200

    create the navigation-bar component

---
+++ src/app/components/navigation-bar/navigation-bar.component.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing/';
+import { MockComponent, routesStub } from 'src/app/routes.stub';
+import { NavigationBarComponent } from './navigation-bar.component';
+
+describe('NavigationBarComponent', () => {
+  let component: NavigationBarComponent;
+  let fixture: ComponentFixture<NavigationBarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        NavigationBarComponent,
+        MockComponent
+      ],
+      imports: [
+        RouterTestingModule.withRoutes(routesStub),
+      ],
+    }).compileComponents();
+
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavigationBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+});
