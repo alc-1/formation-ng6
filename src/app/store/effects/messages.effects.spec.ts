4b055566f90d12a9edcc80d6c56c5cf4fc89c857
Arnaud Béguin <abeguin@octo.com>
2019-04-08 06:57:22 +0200

    create an effect to catch post action an call the message service

--- src/app/store/effects/messages.effects.spec.ts
+++ src/app/store/effects/messages.effects.spec.ts
@@ -3,10 +3,10 @@ import { Http } from '@angular/http';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { cold, hot } from 'jasmine-marbles';
 import { Observable } from 'rxjs';
-import { httpStub } from 'src/app/shared/services/http.stub';
+import { httpStub, httpStubWithError } from 'src/app/shared/services/http.stub';
 import { MessageService } from 'src/app/shared/services/message.service';
 import * as actions from '../actions/messages.actions';
-import { testMessages } from '../state.test.data';
+import { testMessage, testMessages } from '../state.test.data';
 import { MessageEffects } from './messages.effects';
 
 describe('Message Effects', () => {
@@ -14,14 +14,13 @@ describe('Message Effects', () => {
   let effects: MessageEffects;
 
   describe('With healthy http requests', () => {
-
     beforeEach(() => {
       TestBed.configureTestingModule({
         providers: [
           MessageEffects,
           provideMockActions(() => actions$),
           MessageService,
-          {provide: Http, useValue: httpStub}
+          { provide: Http, useValue: httpStub }
         ]
       });
 
@@ -36,10 +35,21 @@ describe('Message Effects', () => {
       const action = new actions.GetAllMessages();
       const completion = new actions.GetAllMessagesSuccess(testMessages);
 
-      actions$ = hot('--a-', { a: action})
+      actions$ = hot('--a-', { a: action });
       const expected = cold('--b', { b: completion });
 
       expect(effects.getAllMessages$).toBeObservable(expected);
     });
+
+    it('PotMessage should complete with GetAllMessageSuccess', () => {
+      const action = new actions.PostMessage(testMessage);
+      const completion = new actions.PostMessageSuccess(testMessage);
+
+      actions$ = hot('--a-', { a: action });
+      const expected = cold('--b', { b: completion });
+
+      expect(effects.postMessage$).toBeObservable(expected);
+    });
   });
+
 });
--- src/app/store/effects/messages.effects.ts
+++ src/app/store/effects/messages.effects.ts
@@ -3,7 +3,12 @@ import { Actions, Effect, ofType } from '@ngrx/effects';
 import { map, switchMap } from 'rxjs/operators';
 import { Message } from 'src/app/shared/models/message.model';
 import { MessageService } from 'src/app/shared/services/message.service';
-import { ActionTypes, GetAllMessagesSuccess } from '../actions/messages.actions';
+import {
+  ActionTypes,
+  GetAllMessagesSuccess,
+  PostMessage,
+  PostMessageSuccess
+} from '../actions/messages.actions';
 
 @Injectable()
 export class MessageEffects {
@@ -13,15 +18,26 @@ export class MessageEffects {
   ) {}
 
   @Effect()
-  getAllMessages$ = this.actions$
-    .pipe(
-      ofType(ActionTypes.GetAllMessages),
-      switchMap(() => {
-        return this.messageService.getMessages()
-          .pipe(map((messages: Message[]) => {
-            return new GetAllMessagesSuccess(messages)
-          }));
-      }),
-    );
-}
+  getAllMessages$ = this.actions$.pipe(
+    ofType(ActionTypes.GetAllMessages),
+    switchMap(() => {
+      return this.messageService.getMessages().pipe(
+        map((messages: Message[]) => {
+          return new GetAllMessagesSuccess(messages);
+        })
+      );
+    })
+  );
 
+  @Effect()
+  postMessage$ = this.actions$.pipe(
+    ofType(ActionTypes.PostMessage),
+    switchMap((action: PostMessage) => {
+      return this.messageService.createMessage(action.message).pipe(
+        map(message => {
+          return new PostMessageSuccess(message);
+        }),
+      );
+    })
+  );
+}
