e94591af4357dfa78a8d5b172e7c1a51a49533e1
Aktar Ibrahim <603229+Aktar@users.noreply.github.com>
2019-04-08 04:43:00 +0200

    01 - Fixed the tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -10,9 +9,8 @@ describe('AppComponent', () => {
         AppComponent
       ],
       imports: [
-        AppRoutingModule
+        RouterTestingModule
       ],
-      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
